49b332af8b1fea3d7aac15f2b42a36f938cf1254
lwzm <lwzm@qq.com>
2020-04-22 11:02:05 +0800

    conda.sh bash.sh

---
+++ bash.sh
@@ -0,0 +1,31 @@
+cat >~/.inputrc <<EOF
+"\C-p": history-search-backward
+"\C-n": history-search-forward
+EOF
+
+cat >~/.bashrc <<EOF
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+alias ll='ls -l'
+alias df='df -h'
+alias du='du -h'
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+function get() {
+    _url=http://fs.tyio.net/$1
+    curl -L $_url
+}
+
+function put() {
+    _url=http://fs.tyio.net/$1
+    _file="-"
+    if [ -f "$1" ]; then
+        _file="$1"
+    fi
+    curl -L -T $_file $_url
+}
+EOF
---
+++ conda.sh
@@ -0,0 +1,2 @@
+wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2020.02-Linux-x86_64.sh
+sh Anaconda3-*.sh -b -f -p /usr/local/
