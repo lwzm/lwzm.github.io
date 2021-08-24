7cb94a5e4ac4b48b22197b4d38ee741fa123db1b
lwzm <lwzm@qq.com>
2021-08-24 09:48:16 +0800

    ulimit -n

--- debian.sh
+++ debian.sh
@@ -4,4 +4,15 @@ LANGUAGE=C.UTF-8
 LC_ALL=C.UTF-8
 EOF
 
+cat >/etc/security/limits.conf <<'EOF'
+root soft nofile 65535
+root hard nofile 65535
+* soft nofile 65535
+* hard nofile 65535
+EOF
+
+cat >>/etc/sysctl.conf <<'EOF'
+net.ipv4.ip_forward = 1
+EOF
+
 sysctl -w net.ipv4.ip_forward=1
